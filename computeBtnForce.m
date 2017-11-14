0c93783653a942af779023d063b1f7e8fc92de75
Zhiheng Zhang <zzh112119@gmail.com>
2017-11-13 20:39:51 -0500

    update

--- computeBtnForce.m
+++ computeBtnForce.m
@@ -20,13 +20,13 @@ if sum(x1 .* X1) >= 0 & sum(x1 .* X2) >= 0 & sum(x4 .* X3) >= 0 & sum(x4 .* X4) 
         ns = -ns;
     end
     if sum(ns .* x1) < 0
-        if norm(ns .* x1) < 30
+        if norm((x1 .* ns/norm(ns)) .^ 2) < 30
             F_btn = norm(ns .* x1) .* sqrt(sum((x1 .* ns/norm(ns)) .^ 2)) * (ns / norm(ns));
         end
-        if norm(ns .* x1)>30 && norm(ns .* x1)<35
+        if norm((x1 .* ns/norm(ns)) .^ 2)>30 && norm((x1 .* ns/norm(ns)) .^ 2)<35
             F_btn=0;
         end
-        if norm(ns .* x1) > 35
+        if norm((x1 .* ns/norm(ns)) .^ 2) > 35
             F_btn = c_surface * sqrt(sum((x1 .* ns/norm(ns)) .^ 2)) * (ns / norm(ns));
             if BtnFlag == 1
                 BtnFlag=0;
